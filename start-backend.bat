9b6c09d695e6bae2e2f455c46df1a6bc6e2667de
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-07-06 18:48:06 +0300

    deleted all useless documents

--- start-backend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Restaurant Ordering System Backend...
-echo.
-cd backend
-npm run dev
--- start-frontend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Restaurant Ordering System Frontend...
-echo.
-cd frontend
-npm run dev
